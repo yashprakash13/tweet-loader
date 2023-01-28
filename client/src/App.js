7024cc164b429422110a1fe813d9c68faa2b94c9
Yash Prakash <yashprakash13@gmail.com>
2023-01-28 15:51:10 -0500

    initial setup.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+import Title from "./components/Title"
+
+function App() {
+  return (
+    <div className="App bg-[#F8B195] w-full h-screen overflow-hidden">
+      <Title />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+  return (
+    <div className="flex flex-row text-4xl font-bold font-cinzel mx-auto mt-10 mb-9 align-items-center justify-center">
+      TweetLoader
+    </div>
+  )
+}
+
+export default Title
