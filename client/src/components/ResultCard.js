9f7d93ff20e4d9f26a05ac95ab8400b9b35e3c2b
Yash Prakash <yashprakash13@gmail.com>
2023-01-29 12:44:16 -0500

    better text formatting with prewrap.

--- client/src/components/ResultCard.js
+++ client/src/components/ResultCard.js
@@ -1,8 +1,10 @@
 const ResultCard = ({ tweet }) => {
   return (
-    <div className="flex justify-center items-center w-[28rem] h-[16rem] m-[1.5rem]">
+    <div className="flex justify-center items-center w-[28rem] max-h-[26rem] m-[1.5rem]">
       <div className="w-full h-full p-6 rounded-[5px] border-primaryText border-2 border-b-4 border-r-4 bg-[#fff] max-w-sm">
-        <p className="text-gray-700 text-base mb-4">{tweet.text}</p>
+        <p className="text-gray-700 text-base mb-4 whitespace-pre-wrap">
+          {tweet.text}
+        </p>
       </div>
     </div>
   )
